077c04018499ed9e124df7a798b80f3a7386ad9c
nexus166 <szampardi@deepnull.com>
2022-01-06 17:13:29 +0100

    init

---
+++ go.mod
@@ -0,0 +1,15 @@
+module vcs.opsz.net/go/srvmon
+
+go 1.17
+
+require (
+	github.com/szampardi/hermes v0.0.3
+	github.com/szampardi/msg v2.4.0+incompatible
+	github.com/szampardi/xprint v1.1.2
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
+)
+
+require (
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
+)
---
+++ main.go
@@ -0,0 +1,195 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/szampardi/hermes"
+	log "github.com/szampardi/msg"
+
+	"github.com/szampardi/xprint/temple"
+	"gopkg.in/yaml.v3"
+)
+
+type (
+	configuration struct {
+		ConcurrentChecks int           `yaml:"ConcurrenctChecks,omitempty"`
+		Timeout          time.Duration `yaml:"Timeout,omitempty"`
+		RetryDelay       time.Duration `yaml:"RetryDelay,omitempty"`
+		RetryAttempts    int           `yaml:"RetryAttempts,omitempty"`
+		Alerts           string        `yaml:"Alerts,omitempty"`
+		PageTitle        string        `yaml:"PageTitle,omitempty"`
+		Targets          []Target      `yaml:"Targets,omitempty"`
+	}
+	Target struct {
+		ID                 string `yaml:"ID,omitempty"`
+		Category           string `yaml:"Category,omitempty"`
+		URL                string `yaml:"URL,omitempty"`
+		Method             string `yaml:"method,omitempty"`
+		ExpectedStatusCode int    `yaml:"statuscode,omitempty"`
+		RetryAttempts      int    `yaml:"RetryAttempts,omitempty"`
+		DNSAddress         string `yaml:"DNSAddress,omitempty"`
+		TLSSkipVerify      bool   `yaml:"TLSSkipVerify,omitempty"`
+		clt                *http.Client
+	}
+)
+
+var (
+	defaultWebHook        string
+	l                     log.Logger
+	logfmt                log.Format = log.Formats[log.PlainFormat]
+	loglvl                log.Lvl    = log.LNotice
+	logcolor                         = flag.Bool("C", false, "colorize output")
+	conf                             = &configuration{}
+	dumpconf              bool
+	confFile              string
+	showVersion           *bool = flag.Bool("V", false, "print build version/date and exit")
+	semver, commit, built       = "v0.0.0-dev", "local", "a while ago"
+)
+
+func init() {
+	temple.FnMap.Fn("checkAll", "", checkAll, false)
+	flag.Func(
+		"F",
+		fmt.Sprintf("logging format (prefix) %v", logFmts()),
+		func(value string) error {
+			if v, ok := log.Formats[value]; ok {
+				logfmt = v
+				return nil
+			}
+			return fmt.Errorf("invalid format [%s] specified", value)
+		},
+	)
+	flag.Func(
+		"L",
+		"log level",
+		func(value string) error {
+			i, err := strconv.Atoi(value)
+			if err != nil {
+				return err
+			}
+			loglvl = log.Lvl(i)
+			return log.IsValidLevel(i)
+		},
+	)
+	flag.IntVar(&conf.ConcurrentChecks, "concurrency", 4, "concurrent checks")
+	flag.DurationVar(&conf.Timeout, "timeout", 10*time.Second, "checks' timeout")
+	flag.DurationVar(&conf.RetryDelay, "delay", 5*time.Second, "delay between failed attempts and retries")
+	flag.IntVar(&conf.RetryAttempts, "retry", 1, "generic retry counter for failed checks")
+	flag.StringVar(&conf.Alerts, "alerts", os.Getenv("WEBHOOK"), "URL to POST alerts to")
+	flag.StringVar(&conf.PageTitle, "title", "Status Dashboard", "webpage title")
+
+	flag.StringVar(&confFile, "config", "", "configuration file to use")
+	flag.BoolVar(&dumpconf, "dump", false, "dump loaded config (including arguments) to yaml")
+
+	for !flag.Parsed() {
+		flag.Parse()
+	}
+	if defaultWebHook != "" {
+		hermes.WebHook = defaultWebHook
+		if conf.Alerts == "" {
+			conf.Alerts = defaultWebHook
+		}
+	}
+	if conf.Alerts != "" {
+		hermes.WebHook = conf.Alerts
+	}
+
+	if *showVersion {
+		fmt.Fprintf(os.Stderr, "github.com/szampardi/srvmon version %s (%s) built %s\n", semver, commit, built)
+		os.Exit(0)
+	}
+
+	var err error
+
+	if err = log.IsValidLevel(int(loglvl)); err != nil {
+		panic(err)
+	}
+	l, err = log.New(logfmt.String(), log.Formats[log.DefTimeFmt].String(), loglvl, *logcolor, "srvmon", os.Stderr)
+	if err != nil {
+		panic(err)
+	}
+
+	if confFile != "" {
+		if b, err := ioutil.ReadFile(confFile); err != nil {
+			l.Panic(err.Error())
+		} else if err := yaml.Unmarshal(b, &conf); err != nil {
+			l.Panic(err.Error())
+		}
+	}
+
+	for _, arg := range flag.Args() {
+		tgt := new(Target)
+		for i, p := range strings.Split(arg, ",") {
+			switch i {
+			case 0:
+				tgt.ID = p
+			case 1:
+				tgt.URL = p
+			case 2:
+				tgt.Category = p
+			case 3:
+				tgt.Method = strings.ToUpper(p)
+			case 4:
+				if sc, err := strconv.Atoi(p); err == nil {
+					tgt.ExpectedStatusCode = sc
+				}
+			case 5:
+				if rt, err := strconv.Atoi(p); err == nil {
+					tgt.RetryAttempts = rt
+				}
+			case 6:
+				tgt.DNSAddress = p
+			case 7:
+				v, err := strconv.ParseBool(p)
+				if err == nil {
+					tgt.TLSSkipVerify = v
+				}
+			}
+		}
+		conf.Targets = append(conf.Targets, *tgt)
+	}
+}
+
+func main() {
+	if dumpconf {
+		if err := yaml.NewEncoder(os.Stdout).Encode(conf); err != nil {
+			l.Panic(err.Error())
+		}
+		os.Exit(0)
+	}
+	tpl, _, err := temple.FnMap.BuildHTMLTemplate(
+		false,
+		conf.PageTitle,
+		"",
+		templates,
+	)
+	if err != nil {
+		l.Panic(err.Error())
+	}
+	if err := tpl.ExecuteTemplate(os.Stdout, "index", conf); err != nil {
+		l.Panic(err.Error())
+	}
+	/*	if err := yaml.NewEncoder(os.Stderr).Encode(checkAll(conf.Targets)); err != nil {
+		l.Panic(err.Error())
+	}*/
+
+}
+
+func logFmts() []string {
+	var out []string
+	for f := range log.Formats {
+		if !strings.Contains(f, "rfc") {
+			out = append(out, f)
+		}
+	}
+	sort.Strings(out)
+	return out
+}
